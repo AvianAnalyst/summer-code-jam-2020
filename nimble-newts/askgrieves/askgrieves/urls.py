610e9ada5ab4dc665b848f594fa9568b6bd47baf
Darren Kam <darrenmkam4150@gmail.com>
2020-08-02 19:32:37 -1000

    Consolidate project files Co-authored-by: Hillo111 <68137793+Hillo111@users.noreply.github.com>

--- nimble-newts/askgrieves/askgrieves/urls.py
+++ nimble-newts/askgrieves/askgrieves/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from chatbot import views
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.test, name='home')
+    path('', include('chatbot.urls'), name='home'),
 ]
--- nimble-newts/askgrieves_updated/chatbot/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class Chatbot(models.Model):
-    name = models.CharField(max_length=500)
-
-    def __str__(self):
-        return f"name={self.name}"
