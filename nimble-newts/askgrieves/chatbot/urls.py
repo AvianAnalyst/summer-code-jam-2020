1f7611c00dac6994ae72aef0294c5ff673d206ca
Darren Kam <darrenmkam4150@gmail.com>
2020-08-03 00:48:34 -1000

    Fix indentation and whitespace

--- nimble-newts/askgrieves/chatbot/urls.py
+++ nimble-newts/askgrieves/chatbot/urls.py
@@ -13,14 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from . import views
 from django.contrib import admin
 from django.urls import path
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.test, name='home'),
-    path('<int:bot_id>', views.lookat)
-
+    path('<int:bot_id>', views.lookat),
 ]
